862b28e69c597f8f0447452ac5b7f3193a2526e0
Miah Masvero <miah120@proton.me>
2025-06-26 14:06:42 -0500

    Copy the enchanting table

---
+++ src/main/java/lunar/tinkerer/ModEnchantingTableBlock.java
@@ -0,0 +1,9 @@
+package lunar.tinkerer;
+
+import net.minecraft.block.EnchantingTableBlock;
+
+public class ModEnchantingTableBlock extends EnchantingTableBlock {
+    public ModEnchantingTableBlock(Settings settings) {
+        super(settings);
+    }
+}
