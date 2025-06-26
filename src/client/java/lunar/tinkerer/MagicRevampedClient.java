6f2afea76f259fe96495719137c5cd5789c465e2
Miah Masvero <miah120@proton.me>
2025-06-26 09:27:15 -0500

    Initial commit

---
+++ src/client/java/lunar/tinkerer/MagicRevampedClient.java
@@ -0,0 +1,10 @@
+package lunar.tinkerer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicRevampedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/lunar/tinkerer/MagicRevampedDataGenerator.java
@@ -0,0 +1,11 @@
+package lunar.tinkerer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MagicRevampedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
