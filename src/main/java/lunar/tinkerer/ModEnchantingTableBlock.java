9ff2e0a1c274d76ed7e58c3dbb840f4b0df503ac
Miah Masvero <miah120@proton.me>
2025-07-02 18:30:14 -0500

    Expand enchanting table bookshelf range

--- src/main/java/lunar/tinkerer/ModEnchantingTableBlock.java
+++ src/main/java/lunar/tinkerer/ModEnchantingTableBlock.java
@@ -5,16 +5,26 @@ import net.minecraft.block.EnchantingTableBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Nameable;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
 public class ModEnchantingTableBlock extends EnchantingTableBlock {
+    public static List<BlockPos> POWER_PROVIDER_OFFSETS = BlockPos
+            .stream(-3, 0, -3, 3, 1, 3)
+            .filter(
+                    pos -> Math.abs(pos.getX()) == 2 || Math.abs(pos.getZ()) == 2
+            ).map(BlockPos::toImmutable).toList();
+
     public ModEnchantingTableBlock(Settings settings) {
         super(settings);
     }
@@ -22,6 +32,14 @@ public class ModEnchantingTableBlock extends EnchantingTableBlock {
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new ModEnchantingTableBlockEntity(pos, state);
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        for (BlockPos blockPos : POWER_PROVIDER_OFFSETS) {
+            if (random.nextInt(16) != 0 || !EnchantingTableBlock.canAccessPowerProvider(world, pos, blockPos)) continue;
+            world.addParticleClient(ParticleTypes.ENCHANT, (double)pos.getX() + 0.5, (double)pos.getY() + 2.0, (double)pos.getZ() + 0.5, (double)((float)blockPos.getX() + random.nextFloat()) - 0.5, (float)blockPos.getY() - random.nextFloat() - 1.0f, (double)((float)blockPos.getZ() + random.nextFloat()) - 0.5);
+        }
     }
 
     @Override
