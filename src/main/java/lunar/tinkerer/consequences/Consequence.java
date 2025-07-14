8f761ce082807880f6cf531767a0539a51cc7869
Miah Masvero <miah120@proton.me>
2025-07-13 21:04:02 -0500

    TWEAKS

--- src/main/java/lunar/tinkerer/consequences/Consequence.java
+++ src/main/java/lunar/tinkerer/consequences/Consequence.java
@@ -38,29 +38,6 @@ public record Consequence(
         );
     }
 
-    //    public static final MapCodec<Consequence> CODEC = RecordCodecBuilder
-    //        .mapCodec(instance -> instance.group(
-    //                Codec.STRING
-    //                        .optionalFieldOf("description", "")
-    //                        .forGetter(consequence -> consequence.description),
-    //                Ingredient.CODEC
-    //                    .fieldOf("decoration")
-    //                    .forGetter(consequence -> consequence.decoration),
-    //                Ingredient.CODEC
-    //                    .fieldOf("decoration")
-    //                    .forGetter(consequence -> consequence.decoration),
-    //                Codec.INT
-    //                    .fieldOf("weight")
-    //                    .forGetter(consequence -> consequence.weight)
-    //                //TODO: Implement consequence effects
-    //            ).apply(instance, Consequence::new)
-    //        );
-    //
-    //    public static final Codec<RegistryEntry<Consequence>> ENTRY_CODEC = RegistryFixedCodec
-    //            .of(ModRegistryKeys.CONSEQUENCE);
-    //    public static final PacketCodec<RegistryByteBuf, RegistryEntry<Consequence>> ENTRY_PACKET_CODEC = PacketCodecs
-    //        .registryEntry(ModRegistryKeys.CONSEQUENCE);
-
     public boolean test(Block block) {
         return this.decoration.test(new ItemStack(block.asItem()));
     }
