8b5498f22df6263cd03748f31cb083b559baca0b
Miah <miah120@proton.me>
2026-06-19 18:13:18 -0500

    WIP: Data driven consequences * TransformArea uses block predicate

--- src/main/java/lunar/tinkerer/consequences/effects/TransformArea.java
+++ src/main/java/lunar/tinkerer/consequences/effects/TransformArea.java
@@ -3,6 +3,7 @@ package lunar.tinkerer.consequences.effects;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import lunar.tinkerer.consequences.ConsequenceEffect;
+import net.minecraft.advancements.criterion.BlockPredicate;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
 import net.minecraft.server.level.ServerLevel;
@@ -14,14 +15,13 @@ import net.minecraft.world.level.block.state.BlockState;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-public record TransformArea(Vec3i start, Vec3i end, BlockState result, Optional<BlockState> from) implements ConsequenceEffect {
+public record TransformArea(Vec3i start, Vec3i end, BlockState result, Optional<BlockPredicate> from) implements ConsequenceEffect {
     public static MapCodec<TransformArea> CODEC = RecordCodecBuilder.mapCodec(
             i -> i.group(
                     Vec3i.CODEC.fieldOf("start_offset").forGetter(TransformArea::start),
                     Vec3i.CODEC.fieldOf("end_offset").forGetter(TransformArea::end),
                     BlockState.CODEC.fieldOf("result").forGetter(TransformArea::result),
-                    //TODO: BlockPredicate
-                    BlockState.CODEC.optionalFieldOf("only_transform").forGetter(TransformArea::from)
+                    BlockPredicate.CODEC.optionalFieldOf("only_transform").forGetter(TransformArea::from)
             ).apply(i, TransformArea::new)
     );
 
@@ -31,7 +31,7 @@ public record TransformArea(Vec3i start, Vec3i end, BlockState result, Optional<
     @Override
     public ItemStack apply(ServerLevel world, BlockPos blockPos, ServerPlayer player, CraftingContainer input, ItemStack stack) {
         getBlockPositions(start, end, blockPos)
-            .filter(pos -> from.filter(f -> world.getBlockState(pos) == f).isPresent())
+            .filter(pos -> from.filter(f -> !f.matches(world, pos)).isEmpty())
             .forEach(pos -> world.setBlockAndUpdate(pos, result));
         return ItemStack.EMPTY;
     }
